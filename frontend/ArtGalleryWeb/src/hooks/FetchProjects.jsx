6ca4299c2ac6d1c646f15536ef845deff8111ff0
ZainabMemon <zainabmemon7335@gmail.com>
2025-03-01 20:15:39 +0500

    Update FetchProjects.jsx

--- frontend/ArtGalleryWeb/src/hooks/FetchProjects.jsx
+++ frontend/ArtGalleryWeb/src/hooks/FetchProjects.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FetchProjects = async () => {
  try{
-    const response = await fetch('http://localhost:4000/api/project/');
+    const response = await fetch('project-management-website-z.vercel.app/api/project/');
     if(!response.ok){
         throw new Error(`HTTP error! status: ${response.status}`);
     }const data = await response.json()
