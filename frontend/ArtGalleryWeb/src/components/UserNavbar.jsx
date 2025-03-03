c642af0b9876ac3f5643eb23fbdc679ed727874c
Uzairmemon10 <um889943@gmail.com>
2025-03-03 21:05:13 +0500

    updated responsiveness

--- frontend/ArtGalleryWeb/src/components/UserNavbar.jsx
+++ frontend/ArtGalleryWeb/src/components/UserNavbar.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
+import React from "react";
 import { useAuth } from "../contexts/AuthContext.jsx";
 import { Button } from "antd";
 
 const UserNavbar = () => {
-     const { userData, logout } = useAuth();
-      const handleLogout = async()=>{
-    await logout()
-  }
+  const { userData, logout } = useAuth();
+  const handleLogout = async () => {
+    await logout();
+  };
   return (
     <div className="user-dashboard-heading-search flex-row">
-    <h2>
-      <strong>Welcome,</strong> {userData.name}!
-    </h2>
-      <Button className='user-nav-btn' onClick={handleLogout}><i class="ri-logout-circle-line"></i></Button> 
-    <div className="user-search-bar flex-row">
-      <i className="ri-search-2-line"></i>
-      <input type="search" placeholder="Search for projects" />
+      <h2>
+        <strong>Welcome,</strong> {userData.name}!
+      </h2>
+      <Button className="user-nav-btn" onClick={handleLogout}>
+        <i className="ri-logout-circle-line"></i>
+      </Button>
+      <div className="user-search-bar flex-row">
+        <i className="ri-search-2-line"></i>
+        <input type="search" placeholder="Search for projects" />
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default UserNavbar
+export default UserNavbar;
